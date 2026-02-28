7bb6f3e7752c49271f60d38ba677a7515e52e661
Gustavo Widonsck <gugawid4@gmail.com>
2026-02-28 08:41:46 -0300

    first commit

---
+++ intro_js/index.js
@@ -0,0 +1,81 @@
+//operadores de comparação por igualdade: == e ===
+console.log|(null == undefined)
+//console.log(11 == [1])
+//console.log(true == 1)
+//console.log(1 === 1)
+//console.log(1 === '1')
+//console.log(1 == '1')
+//console.log(1 === 1)
+//console.log(1 == 1)
+
+
+
+
+// //coerção implicita e explícita
+// const n1 = 2
+// const n2 = '3'
+// const n3 = n1 + n2 
+// console.log(n3)
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+
+
+
+// let a = 2
+// console.log(a)
+// console.log(typeof(a))
+// a ="abc"
+// console.log(a)
+// console.log(typeof(a))
+
+
+
+// e o var?
+//não use
+//içamento (hoisting)
+// var idade = 18
+// console.log('Oi, ${nome}')
+// if(idade >= 18){
+//     var nome = 'João'
+//     console.log("Parabéns, " + nome + ". Você pode dirigir")
+// }
+// console.log('Até, ' + nome)
+
+// var linguagem = 'Javascript'
+// console.log('Aprendendo ' + linguagem)
+// var linguagem = 'Java'
+// console.log('Aprendendo ${linguagem}')
+
+// var c = 2
+// c = 3
+// console.log(c)
+
+
+//declarando variáveis 
+// let a = 2
+// console.log(a)
+// let passouDeAno = true
+// console.log(passouDeAno)
+// a = 3
+// console.log(a)
+// let b 
+// console.log(b)
+// b = 1.5
+// console.log(b)
+// b = "abc"
+// console.log(typeof(b))
+// console.log(b)
+
+  //declarando constantes
+//em Javascript
+//const nome = 'José'
+//console.log(nome)
+//const idade = 17
+//console.log(typeof(idade))
+
+//se fosse Java...
+//String nome = "José"
+//nome = 1
+
+//console.log('Helo, World')
